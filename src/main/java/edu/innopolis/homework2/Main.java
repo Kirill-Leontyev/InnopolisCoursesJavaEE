7c1c4428c5a9cad31c16342b417d02b3aac6faa2
Kirill-Leontyev <kirill-l-a2001@yandex.ru>
2021-09-08 09:43:49 +1000

    Homework #2 bug commit

--- src/main/java/edu/innopolis/homework2/Main.java
+++ src/main/java/edu/innopolis/homework2/Main.java
@@ -1,7 +1,20 @@
 package edu.innopolis.homework2;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Homework 2 again without force");
+        MyArrayList<Integer> array;
+        array = new MyArrayList<>(Integer.class, 3);
+        Iterator iterator = array.iterator();
+        array.add(45);
+        array.add(56);
+        array.add(12);
+        System.out.println(array.length());
+        array.add(99);
+        System.out.println(array.length());
+        array.set(0, 1000);
+        while (iterator.hasNext()) System.out.println(iterator.next());
     }
+
 }
