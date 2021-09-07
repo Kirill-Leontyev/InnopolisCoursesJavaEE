2e013236143044cdad52cd5c89dc70363ecf6a0e
Kirill-Leontyev <kirill-l-a2001@yandex.ru>
2021-09-08 06:44:24 +1000

    Homework #2 commit again

--- src/main/java/edu/innopolis/homework2/Main.java
+++ src/main/java/edu/innopolis/homework2/Main.java
@@ -2,6 +2,6 @@ package edu.innopolis.homework2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Homework 2 ...");
+        System.out.println("Homework 2 again");
     }
 }
