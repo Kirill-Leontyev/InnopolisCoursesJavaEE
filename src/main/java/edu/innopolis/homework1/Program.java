1e401cdc4fb8dd92255a1351fada4ef35404c0f2
Kirill-Leontyev <kirill-l-a2001@yandex.ru>
2021-09-08 09:40:06 +1000

    Homework #1 100% commit

---
+++ src/main/java/edu/innopolis/homework1/Program.java
@@ -0,0 +1,13 @@
+package edu.innopolis.homework1;
+
+public class Program {
+    String name;
+
+    Program(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
