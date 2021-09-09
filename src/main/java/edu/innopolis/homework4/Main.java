5ba48f6bcbc7017e0224f8003f4d98c6457e9a94
Kirill-Leontyev <kirill-l-a2001@yandex.ru>
2021-09-10 04:45:00 +1000

    Homework #4 add Scanner

--- src/main/java/edu/innopolis/homework4/Main.java
+++ src/main/java/edu/innopolis/homework4/Main.java
@@ -2,10 +2,12 @@ package edu.innopolis.homework4;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "aaabbcc";
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next(); //aaabbcc
         Map<Character, Integer> hashMap = new HashMap<>();
         for(char ch : input.toCharArray()) {
             hashMap.merge(ch,1, Integer::sum);
